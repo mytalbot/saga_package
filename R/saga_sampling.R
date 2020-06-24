678bfa0c0100adeec3d51415775a50bfa89ed4ed
Talbot <talbot.steven@mh-hannover.de>
2020-06-24 11:50:18 +0200

    update saga_sampling.R untransforming

--- R/saga_sampling.R
+++ R/saga_sampling.R
@@ -2,7 +2,7 @@
 #' Sample preparation of the joint SAGA data set.
 #'
 #' \code{saga_sampling} will prepare training and testing data of the joint SAGA data set. An internal process
-#' is used to filter all data for candidate probes suited for classification of array data into transforming/nontransforming.
+#' is used to filter all data for candidate probes suited for classification of array data into transforming/untransforming.
 #' The list cannot be changed in this version and the package is optimized for this specific gene set. Also, a PCA plot may be
 #' generated to monitor the generalized context of the sample data within the SAGA training set.
 #'
@@ -10,7 +10,7 @@
 #' @param matrix.test Matrix with user sample or test data
 #'
 #' @return \code{matrix.train} normalized, probe-averaged and batch-corrected SAGA training data.
-#' @return \code{labels.train} class labels (factors) for SAGA training data. Can either be "transforming" or "nontransforming".
+#' @return \code{labels.train} class labels (factors) for SAGA training data. Can either be "transforming" or "untransforming".
 #' @return \code{matrix.unknown} matrix of sample data with array names as row names and probes as column names.
 #'
 #' @export
